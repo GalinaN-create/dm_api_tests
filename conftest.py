c5e7d475f35e8924c1be7a788819ac8fe2258b77
GalinaN-create <78936085+GalinaN-create@users.noreply.github.com>
2023-08-08 01:48:28 +0300

    Создала конфигурациооный файл для своего проекта и вынесла в него все значения переменных(для API, БД, почтового сервера). Создала папку со скрином конфигурационного файла с именем config.png

--- conftest.py
+++ conftest.py
@@ -1,25 +1,23 @@
-import time
 import pytest
-
+from vyper import v
+from pathlib import Path
 from generic.helpers.mailhog import MailhogApi
-
-from dm_api_account.models.registration_model import Registration
-from dm_api_account.models.login_credentials_model import LoginCredentials
-
-import requests
-from generic.helpers.dm_db import DmDatabase
 from generic.helpers.orm_db import OrmDatabase
 from services.dm_api_account import Facade
-from dm_api_account.models.reset_password_model import ResetPassword
-
 import structlog
+from collections import namedtuple
 
 structlog.configure(
     processors=[
         structlog.processors.JSONRenderer(indent=4, sort_keys=True, ensure_ascii=False)
     ]
 )
-from collections import namedtuple
+
+options = (
+    'services.dm_api_account',
+    'services.mailhog',
+    'database.dm3_5.host'
+)
 
 
 @pytest.fixture
@@ -34,17 +32,39 @@ def prepare_user(dm_api_facade, orm_db):
     return User
 
 
+@pytest.fixture(autouse=True)
+def set_config(request):
+    config = Path(__file__).parent.joinpath('config')
+    config_name = request.config.getoption('--env')
+    v.set_config_name(config_name)
+    v.add_config_path(config)
+    v.read_in_config()
+    for option in options:
+        v.set(option, request.config.getoption(f'--{option}'))
+
+
+def pytest_addoption(parser):
+    parser.addoption('--env', action='store', default='stg')
+    for option in options:
+        parser.addoption(f'--{option}', action='store', default=None)
+
+
 @pytest.fixture
 def mailhog():
-    return MailhogApi(host="http://localhost:5025")
+    return MailhogApi(host=v.get('service.mailhog'))
 
 
 @pytest.fixture
 def dm_api_facade(mailhog):
-    return Facade(host="http://localhost:5051", mailhog=mailhog)
+    return Facade(host=v.get('service.dm_api_account'), mailhog=mailhog)
 
 
 @pytest.fixture
 def orm_db():
-    orm = OrmDatabase(user='postgres', password='admin', host='localhost', database='dm3.5')
+    orm = OrmDatabase(
+        user=v.get('database.dm3_5.user'),
+        password=v.get('database.dm3_5.password'),
+        host=v.get('database.dm3_5.host'),
+        database=v.get('database.dm3_5.database')
+    )
     return orm
